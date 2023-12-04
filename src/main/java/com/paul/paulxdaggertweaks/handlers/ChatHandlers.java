5f0844e3d0a5d0aad3e988191423ae5dc6379e5f
tristianthedeveloper <tristianthedeveloper@gmail.com>
2023-12-04 15:05:06 -0800

    we have the image rendering, but some scale or something is completely fucking up, leading some images to only rendering minor parts of them

--- src/main/java/com/paul/paulxdaggertweaks/handlers/ChatHandlers.java
+++ src/main/java/com/paul/paulxdaggertweaks/handlers/ChatHandlers.java
@@ -8,7 +8,16 @@ import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.Tuple;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
@@ -21,7 +30,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid = "paulxdaggertweaks")
 public class ChatHandlers {
 
-	private static final HashMap<String, BufferedImage> linksAndImages = new HashMap<>();
+	private static final HashMap<String, Tuple<BufferedImage, DynamicTexture>> linksAndImages = new HashMap<>();
 
 	private static final Pattern imageUrlRegex = Pattern
 			.compile("(?<link>((http)?s?:?(\\/\\/[^\"']*\\.(?:png|jpg|jpeg|gif|png|svg))))");
@@ -41,6 +50,37 @@ public class ChatHandlers {
 		if (linksAndImages.containsKey(url)) {
 
 			System.out.println("Rendering image for url " + url);
+
+			Tuple<BufferedImage, DynamicTexture> image = linksAndImages.get(url);
+
+			BufferedImage bi = image.getFirst();
+			DynamicTexture dt = image.getSecond();
+
+			int width = bi.getWidth();
+			int height = bi.getHeight();
+
+			System.out.println("image dimensions: " + width + " x " + height);
+			
+			
+			GlStateManager.pushMatrix();
+			
+			GlStateManager.color(1f, 1f, 1f, 1f);
+
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, dt.getGlTextureId());
+			
+			int w;
+
+			Gui.drawModalRectWithCustomSizedTexture(
+					0,
+					0, 
+					0,
+					0, 
+					width,
+					Minecraft.getMinecraft().displayHeight, width, height);
+
+			
+			GlStateManager.popMatrix();
+
 		}
 
 		// this is so fucking scuffed
@@ -64,7 +104,7 @@ public class ChatHandlers {
 
 				try {
 					BufferedImage image = ImageIO.read(new URL(url));
-					linksAndImages.put(url, image);
+					linksAndImages.put(url, new Tuple<BufferedImage, DynamicTexture>(image, new DynamicTexture(image)));
 
 					System.out.println("grabbed image successfully");
 				} catch (Exception $ex) {
@@ -79,6 +119,27 @@ public class ChatHandlers {
 		} else {
 			System.out.println("not image");
 		}
+	}
+
+	private static void drawTexturedModalRect(float xCoord, float yCoord, int minU, int minV, int maxU, int maxV) {
+		float f = 0.00390625F;
+		float f1 = 0.00390625F;
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+		bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+		bufferbuilder.pos((double) (xCoord + 0.0F), (double) (yCoord + (float) maxV), (double) -90)
+				.tex((double) ((float) (minU + 0) * 0.00390625F), (double) ((float) (minV + maxV) * 0.00390625F))
+				.endVertex();
+		bufferbuilder.pos((double) (xCoord + (float) maxU), (double) (yCoord + (float) maxV), (double) -90)
+				.tex((double) ((float) (minU + maxU) * 0.00390625F), (double) ((float) (minV + maxV) * 0.00390625F))
+				.endVertex();
+		bufferbuilder.pos((double) (xCoord + (float) maxU), (double) (yCoord + 0.0F), (double) -90)
+				.tex((double) ((float) (minU + maxU) * 0.00390625F), (double) ((float) (minV + 0) * 0.00390625F))
+				.endVertex();
+		bufferbuilder.pos((double) (xCoord + 0.0F), (double) (yCoord + 0.0F), (double) -90)
+				.tex((double) ((float) (minU + 0) * 0.00390625F), (double) ((float) (minV + 0) * 0.00390625F))
+				.endVertex();
+		tessellator.draw();
 	}
 
 }
